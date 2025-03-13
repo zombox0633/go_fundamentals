74069b7105755c2808399452211d3f7bfcefef8d
Zombox <ghostzb@outlook.com>
2025-03-13 18:31:28 +0700

    add Anonymous function

--- variable/function.go
+++ variable/function.go
@@ -1,11 +1,15 @@
 package variable
 
+import (
+	"reflect"
+)
+
 func showHello() {
 	println("Hello World")
 }
 
 // go ไม่สามารถกำหนด default value ให้กับ parameter ได้
-func plusNumber(a int, b int) int {
+func plusNumber(a, b int) int {
 	value := a + b
 	return value
 }
@@ -15,4 +19,12 @@ func ShowFunction() {
 
 	result := plusNumber(10, 20)
 	println("Result: ", result)
+
+	//Anonymous function
+	isNumber := func(n interface{}) bool { // interface{} คือ type ที่รับค่าอะไรก็ได้
+		return reflect.TypeOf(n).Kind() == reflect.Int
+	}
+
+	result2 := isNumber("Hello")
+	println("Result2: ", result2)
 }
