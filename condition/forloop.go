b80c26cf9cef7625be85b59edaf8e5d241060a08
Zombox <ghostzb@outlook.com>
2025-03-13 18:31:41 +0700

    add ShowForLoop

---
+++ condition/forloop.go
@@ -0,0 +1,9 @@
+package condition
+
+func ShowForLoop() {
+	i := 0
+	for i < 5 {
+		println(i)
+		i++
+	}
+}
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main // กำหนดว่าไฟล์นี้เป็น main package
 import (
 	"fmt"
+
+	"github.com/zombox0633/starter_go/variable"
 )
 
 func main() {
-	fmt.Printf("😿 ")
+	fmt.Printf("😿 : \n")
 
 	// variable.ShowVariable()
 	// variable.ShowArray()
 	// variable.ShowSlice()
 	// variable.ShowMap()
 	// variable.ShowFunction()
-	// variable.ShowPointer()
+	variable.ShowPointer()
 
+	// condition.ShowCondition()
+	// condition.ShowForLoop()
 }
